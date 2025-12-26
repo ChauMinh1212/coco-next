4749a98d6b7430fa4e9c6e7d3cd21a818139b2bc
Chau Minh <chauminh.12122000@gmail.com>
2025-12-26 15:18:14 +0700

    chore: update footer layout and contact information

--- src/lib/components/layout/Footer.tsx
+++ src/lib/components/layout/Footer.tsx
@@ -9,19 +9,19 @@ export const Footer = () => {
                     <p><span className="font-semibold mr-[3px]">Liên hệ:</span><a href="tel:+84967387309">0967 387 309</a> (Phương Nam)</p>
                     <p><span className="font-semibold mr-[3px]">Zalo:</span><a href="https://zalo.me/0967387309" target="_blank">0967 387 309</a> (Phương Nam)</p>
                     {/* <p><span className="font-semibold mr-[3px]">Định vị:</span><a target="_blank" href="https://maps.app.goo.gl/pgvLoQA7wVV4ECJ69">COCO GARDEN (Phà Cát Lái, Phú Hữu, Nhơn Trạch, Đồng Nai)</a></p> */}
-                    <p><span className="font-semibold mr-[3px]">Định vị:</span><a target="_blank" href="https://www.google.com/maps">COCO GARDEN (Phà Cát Lái, Phú Hữu, Nhơn Trạch, Đồng Nai)</a></p>
-                    <p><span className="font-semibold mr-[3px]">Gmail:</span><a href="mailto:cocogarden.dn@gmail.com">cocogarden.dn@gmail.com</a></p>
-                </div>
-                <div className="w-fit relative text-4xl sm:text-5xl">
-                    <div className="flex mb-4 sm:mb-5">
-                        <p className="font-frank text-white text-line-[1.5px_rgb(0,0,0)]">COCO</p>
-                        <div className="w-[35px] sm:w-[43px] md:w-[36px]">
-                            <ImageCustom src="/img/coconut.gif" alt="" unoptimized={true} />
-                        </div>
+                    <p><span className="font-semibold mr-[3px]">Định vị:</span>COCO GARDEN (Phà Cát Lái, Phú Hữu, Nhơn Trạch, Đồng Nai)</p>
+                <p><span className="font-semibold mr-[3px]">Gmail:</span><a href="mailto:cocogarden.dn@gmail.com">cocogarden.dn@gmail.com</a></p>
+            </div>
+            <div className="w-fit relative text-4xl sm:text-5xl">
+                <div className="flex mb-4 sm:mb-5">
+                    <p className="font-frank text-white text-line-[1.5px_rgb(0,0,0)]">COCO</p>
+                    <div className="w-[35px] sm:w-[43px] md:w-[36px]">
+                        <ImageCustom src="/img/coconut.gif" alt="" unoptimized={true} />
                     </div>
-                    <p className="font-frank text-[#ffde59] text-line-[1.5px_rgb(0,0,0)] mt-[-20px] sm:mt-[-20px]">GARDEN</p>
                 </div>
+                <p className="font-frank text-[#ffde59] text-line-[1.5px_rgb(0,0,0)] mt-[-20px] sm:mt-[-20px]">GARDEN</p>
             </div>
         </div>
+        </div >
     )
 }
